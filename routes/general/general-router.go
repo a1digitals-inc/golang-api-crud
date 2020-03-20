405846336a874358d682fd32cf4ede37aa692664
Asif Iqbal <asifnitsxr@gmail.com>
2020-03-20 12:12:59 +0530

    Refactor general controller

--- routes/general/general-router.go
+++ routes/general/general-router.go
@@ -1,105 +1,92 @@
-package routes
+package generalrouter
 
 import (
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+
+	generalController "github.com/asif-ir/golang-api-crud/controllers/general"
 )
 
-type Post struct {
-  Id string `json:"id"`
-  Title string `json:"title"`
-}
-
+// Response ...
 type Response struct {
-  Message string `json:"message"`
+	Message string `json:"message"`
 }
 
-var posts = []Post {
-  {
-    Id: "1",
-    Title: "Post 1",
-  },
-  {
-    Id: "2",
-    Title: "Post 2",
-  },
-}
-
+// Index ...
 // @Summary Get all posts
 // @Accept multipart/form-data
 // @Produce json
 // @Router / [get]
-func Index (c *gin.Context) {
-	c.JSON(200, posts)
+func Index(c *gin.Context) {
+	c.JSON(200, generalController.Posts)
 }
 
+// Save ...
 // @Summary Save post
 // @Description Save a new post
 // @Produce json
 // @Router / [post]
-func Save (c *gin.Context) {
-  var post Post
-  c.BindJSON(&post)
-  posts = append(posts, post)
-  c.JSON(200, Response {
-    Message: "Success",
-  })
+func Save(c *gin.Context) {
+	var post generalController.Post
+	c.BindJSON(&post)
+	generalController.AddPost(post)
+	c.JSON(200, Response{
+		Message: "Success",
+	})
 }
 
+// Get ...
 // @Summary Get post
 // @Description Get a post with given ID
 // @Produce json
 // @Router /{id} [get]
-func Get (c *gin.Context) {
-  postId := c.Param("id")
-  for _, post := range posts {
-    if post.Id == postId {
-      c.JSON(200, post)
-      return
-    }
-  }
-  c.JSON(404, Response {
-    Message: "Post not found",
-  })
+func Get(c *gin.Context) {
+	postID := c.Param("id")
+	post, err := generalController.GetPost(postID)
+	if err != nil {
+		c.JSON(404, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, post)
 }
 
+// Delete ...
 // @Summary Delete post
 // @Description Delete a post with given ID
 // @Produce json
 // @Router /{id} [delete]
-func Delete (c *gin.Context) {
-  postId := c.Param("id")
-  for i, post := range posts {
-    if post.Id == postId {
-      posts = append(posts[:i], posts[i+1:]...)
-      c.JSON(200, Response {
-        Message: "The post with ID " + postId + " has been deleted successfully",
-      })
-      return
-    }
-  }
-  c.JSON(404, Response {
-    Message: "Post not found",
-  })
+func Delete(c *gin.Context) {
+	postID := c.Param("id")
+	err := generalController.DeletePost(postID)
+	if err != nil {
+		c.JSON(404, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, Response{
+		Message: "The post with ID " + postID + " has been deleted successfully",
+	})
 }
 
+// Update ...
 // @Summary Update post
 // @Description Update a post with given ID
 // @Produce json
 // @Router /{id} [put]
-func Update (c *gin.Context) {
-  postId := c.Param("id")
-  var updatedPost Post
-  c.BindJSON(&updatedPost)
-  for i, post := range posts {
-    if post.Id == postId {
-      posts[i].Title = updatedPost.Title
-      c.JSON(200, Response {
-        Message: "The post with ID " + postId + " has been updated successfully",
-      })
-      return
-    }
-  }
-  c.JSON(404, Response {
-    Message: "Post not found",
-  })
+func Update(c *gin.Context) {
+	postID := c.Param("id")
+	var updatedPost generalController.Post
+	c.BindJSON(&updatedPost)
+	err := generalController.UpdatePost(postID, updatedPost)
+	if err != nil {
+		c.JSON(404, Response{
+			Message: err.Error(),
+		})
+		return
+	}
+	c.JSON(200, Response{
+		Message: "The post with ID " + postID + " has been updated successfully",
+	})
 }
